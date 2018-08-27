271654f367f89097c92ae871b7f371ee37d48206
Marly Cormar <marlycormar@gmail.com>
2018-08-26 21:09:41 -0400

    Add comments to the code.

--- graph.R
+++ graph.R
@@ -6,6 +6,7 @@ smallest_prime_to_consider <- 2
 max_k <- 20 # Max k for which to find U_k
 max_prime <- 200 # max prime to consider
 
+# Find U_k for each k in [1:max_k] and make a plot
 set_of_union_of_sets_of_lengths <- find_set_of_union_of_sets_of_lengths(max_k)
 print(set_of_union_of_sets_of_lengths)
 draw_plot(set_of_union_of_sets_of_lengths)
